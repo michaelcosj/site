8acb86c2561520b7f44d5ca2a3d2d3edbeff107b
michael osajeh <michaelcosj@proton.me>
2024-01-03 02:07:21 +0100

    fixed minor issue in the rss xml

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,15 @@
-// import { dev } from "$app/environment";
+import { dev } from "$app/environment";
 
 export const title = "Garlic";
 export const description = "Michael's personal corner of da web";
-export const url = "http://localhost:5173";
+export const url = dev
+	? "http://localhost:5173"
+	: "https://michaelcosj.pages.dev";
 
 export const projects = [
 	{
 		name: "Mediocre Version Control System",
-        url: "https://github.com/michaelcosj/mvcs",
+		url: "https://github.com/michaelcosj/mvcs",
 		techUsed: ["golang", "git", "version control"],
 	},
 ];
