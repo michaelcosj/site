d39f9097e8d7c49fc7a766192c9214d16e68ab21
michael <michaelcosj@proton.me>
2026-01-25 15:19:34 +0100

    style: improve light mode support with new grid variant

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 <Nav />
 
 <main class="relative md:px-24">
-  <div class="pointer-events-none fixed inset-0 h-full w-full bg-[url(/grid.svg)] mask-linear-[white_75%,transparent_100%]"></div>
+  <div class="pointer-events-none fixed inset-0 h-full w-full bg-[url(/grid_light.svg)] dark:bg-[url(/grid.svg)] dark:mask-linear-[white_75%,transparent_100%] mask-linear-[black_75%,transparent_100%]"></div>
   {@render children?.()}
 </main>
 
